0dc744d50cd583e83deb8f04c6f17ecc10de6b15
Luis Pablo <luispablo@MacBook-Air-de-Luis.local>
2022-12-01 12:29:06 +0100

    FEAT: Adding digit container and some styles

---
+++ main.js
@@ -0,0 +1,5 @@
+import Digito  from './digito/digito.js';
+import ContenedorDigitos from './contenedor_digitos/contenedorDigitos.js'
+
+window.customElements.define('wc-digito', Digito);
+window.customElements.define('wc-contenedor-digitos', ContenedorDigitos);
